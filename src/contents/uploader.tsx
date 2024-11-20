4221eb61880332955c9014e92f5b79fb78cf1de8
yoshiaki.inoue <yoshiaki.inoue@nijibox.co.jp>
2024-11-20 10:01:53 +0900

    feat: HTMLファイルをアップロードしメール本文を上書きする機能を追加

---
+++ src/contents/uploader.tsx
@@ -0,0 +1,58 @@
+import Encoding from "encoding-japanese"
+import type { PlasmoCSConfig } from "plasmo"
+
+const GMAIL_BODY_SELECTOR = ".Am.Al.editable"
+
+export const config: PlasmoCSConfig = {
+  matches: ["https://mail.google.com/mail/*"]
+}
+
+const Uploader = () => {
+  const loadFile: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const file = e.target.files?.[0]
+    if (!file) {
+      return
+    }
+
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      if (!e.target?.result || typeof e.target.result === "string") {
+        return
+      }
+      const target = document.querySelector(GMAIL_BODY_SELECTOR)
+      if (!target) {
+        return
+      }
+
+      const codes = new Uint8Array(e.target.result)
+      const unicodeString = Encoding.convert(codes, {
+        to: "UNICODE",
+        type: "string"
+      })
+
+      target.innerHTML = unicodeString
+    }
+    reader.readAsArrayBuffer(file)
+  }
+
+  return (
+    <label
+      style={{
+        position: "fixed",
+        left: "4px",
+        bottom: "4px",
+        borderRadius: "2px",
+        backgroundColor: "white",
+        padding: "8px 16px",
+        display: "inline-block"
+      }}>
+      <input
+        type="file"
+        onChange={loadFile}
+        style={{ display: "none" }}></input>
+      insert HTML mail
+    </label>
+  )
+}
+
+export default Uploader
